6d3c09bb78fe49698e18c769192e199e1efe297e
Jack Partridge <59940643+JackPartridge@users.noreply.github.com>
2020-11-11 23:01:23 +0000

    Add files via upload

---
+++ com.jackpartridge.tileMazeGameV2/src/Tile.java
@@ -0,0 +1,19 @@
+import processing.core.PApplet;
+
+public class Tile extends PApplet{
+
+    public int i;
+    public int j;
+    public boolean[] walls;
+    public boolean visited;
+    public boolean drawn;
+
+    public Tile(int i, int j) {
+        this.i = i;
+        this.j = j;
+        this.visited = false;
+        this.walls = new boolean[]{ true, true, true, true };
+        this.drawn = false;
+
+    }
+}
